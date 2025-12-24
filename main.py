aacbacbe67a526236b7d447a2bf46e4df4de1484
lihe <lihellowd@outlook.com>
2025-12-24 21:08:24 +0800

    Update: 修复仓库名配置，生成 README.md 和 feed.xml

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def add_md_firends(repo, md, me):
     s = markdown.markdown(s, output_format="html", extensions=["extra"])
     with open(md, "a+", encoding="utf-8") as md:
         md.write(
-            f"## [友情链接](https://github.com/{str(me)}/{repo.name}/issues/{friends_issue_number})\n"
+            f"## [友情链接](https://github.com/{str(me)}/leetcode/issues/{friends_issue_number})\n"
         )
         md.write(s)
         md.write("\n\n")
